fff3a2afb7e38f414ac6b81bd5065c0383682ddd
Stefan Mitov <stefan.mitov@clearroute.io>
2024-06-16 06:01:14 +0300

    fix python script call

--- src/python-scripts.js
+++ src/python-scripts.js
@@ -13,13 +13,58 @@ const invokeScript = (script, args) => new Promise((resolve, reject) => {
   });
 });
 
-const predict = async ({ temperature, rainfall, humidity, tire_make, tire_model }) => {
+const predict = async ({
+  thread_depth,
+  tire_type,
+  tire_width,
+  tire_diameter,
+  tire_ratio,
+  car_weight,
+  pressure_checks_frequency,
+  city_avg_speed,
+  outside_city_avg_speed,
+  driving_style,
+  paved_road,
+  offroad,
+  paved_road_quality,
+  offroad_quality,
+  min_temperature,
+  max_temperature,
+  avg_temperature,
+  driving_frequency,
+  car_extra_load_weight,
+  tire_age,
+  distance_driven_with_tires
+}) => {
   const scriptPath = path.join(__dirname, '../model/venv/bin/python');
   const filePath = path.join(__dirname, '../model/predict.py');
 
   try {
 
-    return await invokeScript(scriptPath, [filePath, temperature, rainfall, humidity, tire_make, tire_model]);
+    return await invokeScript(scriptPath, [
+      filePath,
+      thread_depth,
+      tire_type,
+      tire_width,
+      tire_diameter,
+      tire_ratio,
+      car_weight,
+      pressure_checks_frequency,
+      city_avg_speed,
+      outside_city_avg_speed,
+      driving_style,
+      paved_road,
+      offroad,
+      paved_road_quality,
+      offroad_quality,
+      min_temperature,
+      max_temperature,
+      avg_temperature,
+      driving_frequency,
+      car_extra_load_weight,
+      tire_age,
+      distance_driven_with_tires
+    ]);
   } catch (error) {
     console.log(error)
     return error;
