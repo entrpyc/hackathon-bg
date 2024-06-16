ee2d8997f5eb59516968ba5f229f7094197f4914
Stefan Mitov <stefan.mitov@clearroute.io>
2024-06-16 11:39:59 +0300

    fix host

--- api.py
+++ api.py
@@ -47,4 +47,4 @@ async def predict(
     return {"result": float(result)}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
